c85adc967b1d1635fdec939bef47392734335fa9
ryan <ryanjamesdillon@gmail.com>
2015-05-15 15:27:58 +0200

    using phobson/process_cursor.py function

--- access2py.py
+++ access2py.py
@@ -3,42 +3,51 @@
 
 import pypyodbc
 
+def pyodbc_pandas(cursor):
+  '''Return numpy dtype array from pypyodbc cursor with SQL query'''
+  import numpy as np
+  import pandas as pd
+  import datetime
+
+  colinfo = cursor.description
+  dtypes = []
+  for col in colinfo:
+      if col[1] == unicode:
+          dtypes.append((col[0], 'U%d' % col[3]))
+      elif col[1] == str:
+          dtypes.append((col[0], 'S%d' % col[3]))
+      elif col[1] == float:
+          dtypes.append((col[0], 'f4'))
+      elif col[1] == datetime.datetime:
+          dtypes.append((col[0], 'O4'))
+      elif col[1] == int:
+          dtypes.append((col[0], 'i4'))
+
+  #dtypes = np.dtype(zip(column_names,dtypes))
+  dtypes = np.dtype(dtypes)
+  data = np.array(cursor.fetchall(), dtype=dtypes)
+
+  data_frame = pd.DataFrame.from_records(data)
+
+  return data_frame
+
+
 connection = pypyodbc.win_connect_mdb('C:/Users/ryan/Desktop/hg37.mdb')
 cursor = connection.cursor()
 
 mdb_tables = ('cruise','ctd','deployments','diag','dive','haulout','haulout_orig','summary','tag_info','uplink')
 
-def get_dtypes(cursor):
-  '''Return numpy dtype array from pypyodbc cursor with SQL query'''
-  import numpy as np
-  column_names = [column[0] for column in cursor.description]
-  column_dtypes = [column[1] for column in cursor.description]
-  dtypes = np.dtypes = zip(column_names,column_dtype)
-  return dtypes
-
-# Grant permission to user Admin for MSysObjects.Name reading
-#cursor.execute("GRANT SELECT ON MSysObjects TO Admin;")
-
-# Get table names from MSysObjects.Name
-#print cursor.execute("SELECT MSysObjects.Name AS table_name "+
-#                     "FROM MSysObjects "+
-#                     "WHERE (((Left([Name],1))<>'~') "+
-#                     "AND ((Left([Name],4))<>'MSys') "+
-#                     "AND ((MSysObjects.Type) In (1,4,6))) "+
-#                     "order by MSysObjects.Name;")
-
-for table in mdb_tables:
+#for table in mdb_tables:
   # If pandas/pickly object doesn't exist, create/
 
   # Query for by table name
-  cursor.execute('Select * from '+table+';')
+  #cursor.execute('Select * from '+table+';')
+cursor.execute('Select * from cruise;')
+
+data_frame = pyodbc_pandas(cursor)
 
   # Create pandas array for table
 
   # Pickle/bin
-  while True:
-      row = cursor.fetchone()
-      if row is None:
-          break
-      print row[0]
+
 connection.close()
