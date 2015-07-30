4665f0e1fd3c85c7a26b26368e9895fb1cd6d92c
ryan <ryanjamesdillon@gmail.com>
2015-07-30 13:49:42 +0200

    working, strange output from panoply, and very large size afer dive table processing

--- access2py.py
+++ access2py.py
@@ -1,7 +1,11 @@
 # import msdb to python data object
 # http://stackoverflow.com/questions/17910657/pyodbc-error-of-data-source-name-not-found-or-no-default-driver-on-64-bit-pc
 
+import os
 import pypyodbc
+import h5py, tables
+import pandas as pd
+import numpy as np
 
 def cursor_dtypes(cursor):
     import numpy as np
@@ -9,98 +13,115 @@ def cursor_dtypes(cursor):
     import datetime
 
     dtypes = []
+    dtypes_str = []
     column_info = cursor.description
 
     for column in column_info:
         if column[1] == unicode:
             if column[3] > 1000:
-                dtypes.append((column[0], 'U20000'))
+                dtypes.append((column[0], 'S20000'))
+                dtypes_str.append('list')
             else:
-                dtypes.append((column[0], 'U%d' % column[3]))
-
+                dtypes.append((column[0], 'S%d' % column[3]))
+                dtypes_str.append('str')
         elif column[1] == str:
             dtypes.append((column[0], 'S%d' % column[3]))
+            dtypes_str.append('str')
         elif column[1] == decimal.Decimal:
-            dtypes.append((column[0], 'dd%d' % column[3]))
+            #dtypes.append((column[0], 'dd%d' % column[3]))
+            dtypes.append((column[0], 'f%d' % column[3]))
+            dtypes_str.append('dd')
         elif column[1] == float:
             dtypes.append((column[0], 'f4'))
+            dtypes_str.append('f')
         elif column[1] == datetime.datetime:
-            dtypes.append((column[0], 'O4'))
+            dtypes.append((column[0], 'S10'))
+            dtypes_str.append('datetime')
         elif column[1] == int:
             dtypes.append((column[0], 'i4'))
+            dtypes_str.append('i')
 
-    return dtypes
-
-def typecast_data(data, dtypes):
-    import numpy as np
-
-    print 'tc1'
-    data = np.array(data)
-
-    print 'tc2'
-    dtypes = np.array(dtypes)
-
-    print 'tc3'
-    data_y, data_x = data.shape
-
-    for i in range(data_x):
-        if 'U' in dtypes[i,1]:
-            for j in range(data_y):
-                try:
-                    data[j,i] = data[j,i].encode('utf-8')
-                except:
-                    data[j,i] = ''
-            dtypes[i,1] = dtypes[i,1].replace('U','S')
-        elif 'dd' in dtypes[i,1]:
-            for j in range(data_y):
-                try:
-                    data[j,i] = float(data[j,i])
-                except:
-                    data[j,i] = np.nan
-            dtypes[i,1] = 'f4'
-
-    print 'tc4'
-    dtypes = list(map(tuple,dtypes))
-    #data = list(map(tuple,data))
-    #print 'len data '+str(len(data))
-
-    print 'tc5'
     dtypes = np.dtype(dtypes)
-    print data
-    print dtypes
-    #data.astype(dtypes)
-    #data = np.array(data, dtype=dtypes)
 
-    return data
+    return dtypes, dtypes_str
 
-def process_cursor(cursor, data_frame=False):
+def create_table_class(table_obj, dtypes):
+
+    columns = {}
+
+    for dtype in dtypes:
+        item_len = int(dtype[1].translate(None,'USOdfi'))
+        if ('S' in dtype[1]) or ('U' in dtype[1]):
+            columns[dtype[0]] = tables.StringCol(item_len)
+        elif 'i' in dtype[1]:
+            columns[dtype[0]] = tables.Int32Col()
+        elif ('f' in dtype[1]) or ('dd' in dtype[1]):
+            columns[dtype[0]] = tables.Float64Col()
+        elif 'O' in dtype[1]:
+            columns[dtype[0]] = tables.Time64Col()
+
+    table_obj.__module__ = 'tables.description'
+    table_obj.columns = columns
+
+    return table_obj, columns
+
+def process_cursor(cursor, h5file, table):
     '''Return numpy dtype array from pypyodbc cursor with SQL query'''
-    import numpy as np
-    import pandas as pd
+
+    # Query for by table name
+    cursor.execute('Select * from '+table+';')
 
     print 'pc1'
-    dtypes = cursor_dtypes(cursor)
+    dtypes, dtypes_str = cursor_dtypes(cursor)
 
     print 'pc2'
-    data = typecast_data(cursor.fetchall(), dtypes)
+    group_h5 = h5file.create_group(table)
+    print group_h5
 
+    # Get number of rows
+    num_rows = len(cursor.fetchall())
+
+    for i in range(len(dtypes)):
+        param = h5file.create_dataset(table+'/'+ dtypes.names[i],
+                                      (num_rows,),
+                                      dtype = dtypes[i])
     print 'pc3'
-    if data_frame:
-        output = pd.DataFrame.from_records(data)
-    else:
-        output = data
 
-    return output
+    # Query by table name
+    cursor.execute('Select * from '+table+';')
+
+    # Typecast and save data
+    for i in range(num_rows):
+        data = cursor.fetchone()
+        for j in range(len(dtypes)):
+
+            key_h5 = table + '/' + dtypes.names[j]
+
+            if data != None:
+                if ('S' in dtypes.descr[j][1]) or \
+                   ('U' in dtypes.descr[j][1]):
+                    if data[j] == None:
+                        data_str = ''
+                    elif dtypes_str[j] == 'datetime':
+                        data_str = data[j].strftime('%Y-%m-%d')
+                    else:
+                        data_str = data[j].encode('utf-8')
+
+                    h5file[key_h5][i] = data_str
+
+                elif 'f' in dtypes.descr[j][1]:
+                    try:
+                        h5file[key_h5][i] = float(data[j])
+                    except:
+                        h5file[key_h5][i] = -9999
+                else:
+                    h5file[key_h5][i] = data[j]
 
 if __name__ == '__main__':
-    import pandas as pd
-    import os
-    import numpy as np
-    import tables
 
     msdb_file = 'C:/Users/ryan/Desktop/hg37g.mdb'
     msdb_name = os.path.splitext(os.path.split(msdb_file)[1])[0]
-    bin_file = os.path.splitext(msdb_file)[0]+'.h5'
+    bin_file = 'E:/access2py/'+msdb_name+'.h5'
 
     #msdb_tables = ('cruise','ctd','deployments','diag','dive','haulout',
     #              'haulout_orig','summary','tag_info','uplink')
@@ -109,31 +130,23 @@ if __name__ == '__main__':
                   'haulout_orig','sms','summary','tag_info','uplink')
 
     # If pandas/pickly object doesn't exist, create
-    try:
-        #panel = pd.read_pickle(bin_file)
-        #data_db = np.read(bin_file)
-        h5file = tables.openFile(bin_file, mode='r', title=msdb_name)
-        table = h5file.root.detector.readout
-
-    except:
+    if not os.path.isfile(bin_file):
         # Connect to MS Access database & create cursor
         connection = pypyodbc.win_connect_mdb(msdb_file)
         cursor = connection.cursor()
 
-        p = {}
+        #h5file = tables.openFile(bin_file, mode='w', title=msdb_name)
+
+        h5file = h5py.File(bin_file, 'w')
+
         for table in msdb_tables:
 
             print table+'1'
-            # Query for by table name
-            cursor.execute('Select * from '+table+';')
 
-            print table+'2'
-            # Create pandas array for table
-            data_frame = process_cursor(cursor, data_frame=True)
+            # Append table to HDF5 File
+            process_cursor(cursor, h5file, table)
 
-            # Add table dataframe to dictionary
-            p[table] = data_frame
-
+        h5file.close()
         connection.close()
 
         # Create Pandas panel data object
@@ -144,8 +157,5 @@ if __name__ == '__main__':
         #panel.to_pickle(bin_file)
         #np.save(bin_file, p)
 
-        h5file = tables.openFile(bin_file, mode='w', title=msdb_name)
-        root = h5file.root
-        h5file.createArray(root, msdb_name, p)
-
-    h5file.close()
+    else:
+        print bin_file+' already created.'
